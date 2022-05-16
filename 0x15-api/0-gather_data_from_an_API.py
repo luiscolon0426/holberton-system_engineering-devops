a3fd5ee00f6eeb556a7e2845463b03924556fa59
Luis Colon <luiscolon0426@gmail.com>
2022-05-16 18:40:01 -0400

    Add file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# Write a Python script that, using this REST API
-# for a given employee ID, returns information 
-# about his/her TODO list progress.
+''' comment '''
 
 import requests
 from sys import argv
