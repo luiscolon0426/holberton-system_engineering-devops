faac004013596e6611d14cad9a002e31651f89d9
Luis Colon <luiscolon0426@gmail.com>
2022-05-31 13:42:10 -0400

    Fix long line

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,12 +9,12 @@ def recurse(subreddit, hot_list=[], after=None):
     """returns a list of all hot post titles for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return None
-    request = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
-                           headers={'User-Agent': 'Python/requests:APIproject:\
+    req = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                       headers={'User-Agent': 'Python/requests:APIproject:\
                      v1.0.0 (by /u/aaorrico23)'},
-                           params={'after': after}).json()
-    after = request.get('data', {}).get('after', None)
-    posts = request.get('data', {}).get('children', None)
+                       params={'after': after}).json()
+    after = req.get('data', {}).get('after', None)
+    posts = req.get('data', {}).get('children', None)
     if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
         if len(hot_list) == 0:
             return None
